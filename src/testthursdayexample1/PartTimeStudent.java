4fedc4c92048d36148d0a4df853c58feb7845677
sasidhsa <sasidhsa@sheridancollege.ca>
2024-05-23 22:23:29 -0400

    branchversion2

---
+++ src/testthursdayexample1/PartTimeStudent.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testthursdayexample1;
+
+/**
+ *
+ * @author SREEJITH PANIKAR
+ */
+public class PartTimeStudent {
+    private int sid;
+    
+    
+}
